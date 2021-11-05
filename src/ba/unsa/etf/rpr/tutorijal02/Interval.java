42b5deaea6617b4336bb2b1055389b0edef888af
bazman11 <bazman1@etf.unsa.ba>
2021-11-05 10:22:31 +0100

    Uradjen zadatak

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,34 +1,41 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
-import java.util.Objects;
+import java.lang.IllegalArgumentException;
 
 public class Interval {
     double pocetna, krajnja;
     boolean pocetnaPripada, krajnjaPripada;
 
-    public Interval(double pocetna, double krajnja, boolean pocetnaPripada, boolean krajnjaPripada){
-        if (pocetna>krajnja) throw new IllegalArgumentException("Pocetna vrijednost je veca od krajnje");
-        this.pocetna=pocetna;
-        this.krajnja=krajnja;
-        this.pocetnaPripada=pocetnaPripada;
-        this.krajnjaPripada=krajnjaPripada;
+    public Interval(double pocetna, double krajnja, boolean pocetnaPripada, boolean krajnjaPripada) throws IllegalArgumentException {
+        if(pocetna > krajnja) throw new IllegalArgumentException("Pocetna tacka ne moze biti veca od krajnje!!");
+        this.pocetna = pocetna;
+        this.krajnja = krajnja;
+        this.pocetnaPripada = pocetnaPripada;
+        this.krajnjaPripada = krajnjaPripada;
     }
 
-    public Interval(){
-        this.pocetna=0;
-        this.krajnja=0;
-        pocetnaPripada=false;
-        krajnjaPripada=false;
+    public Interval() {
+        this.pocetna = 0;
+        this.krajnja = 0;
+        this.pocetnaPripada = false;
+        this.krajnjaPripada = false;
     }
 
-    boolean isNull(){
-        return this.pocetna==0 && this.krajnja==0 && !this.pocetnaPripada && this.krajnjaPripada;
+    @Override
+    public String toString() {
+        if(this.pocetna == 0 && this.krajnja ==0 && !this.pocetnaPripada) return ("()");
+        if(this.pocetnaPripada && this.krajnjaPripada) return ("[" + this.pocetna + "," + this.krajnja + "]");
+        if(this.pocetnaPripada) return ("[" + this.pocetna + "," + this.krajnja + ")");
+        if(this.krajnjaPripada) return ("(" + this.pocetna + "," + this.krajnja + "]");
+        return ("(" + this.pocetna + "," + this.krajnja + ")");
+    }
+    public boolean isNull(){
+        return this.pocetna == 0 && this.krajnja == 0 && !this.pocetnaPripada && !this.krajnjaPripada;
     }
 
-    boolean isIn(double tacka){
-        if (tacka>this.pocetna && tacka <this.krajnja) return true;
-        if (tacka==this.pocetna && this.pocetnaPripada) return true;
-        return tacka==this.krajnja && this.krajnjaPripada;
+    public boolean isIn(double tacka){
+        if((tacka > this.pocetna && tacka < this.krajnja) || (tacka == this.pocetna && this.pocetnaPripada)) return true;
+        return tacka == this.krajnja && this.krajnjaPripada;
     }
 
     public Interval intersect(Interval i) {
@@ -59,54 +66,4 @@ public class Interval {
         return this;
     }
 
-    public static Interval intersect(Interval j, Interval i) {
-        if ((i.pocetna > j.pocetna) && (i.krajnja < j.krajnja) && (i.pocetnaPripada && i.krajnjaPripada))
-            return new Interval(i.pocetna, i.krajnja, true, true);
-        if ((i.pocetna > j.pocetna) && (i.krajnja < j.krajnja) && (!i.pocetnaPripada && !i.krajnjaPripada))
-            return new Interval(i.pocetna, i.krajnja, false, false);
-        if (i.pocetna > j.pocetna && i.krajnja < j.krajnja && !i.pocetnaPripada && i.krajnjaPripada)
-            return new Interval(i.pocetna, i.krajnja, false, true);
-        if (i.pocetna > j.pocetna && i.krajnja < j.krajnja && i.pocetnaPripada && !i.krajnjaPripada)
-            return new Interval(i.pocetna, i.krajnja, true, true);
-        if (i.pocetna > j.pocetna && i.krajnja > j.krajnja && i.pocetnaPripada && j.krajnjaPripada)
-            return new Interval(i.pocetna, j.krajnja, true, true);
-        if (i.pocetna > j.pocetna && i.krajnja > j.krajnja && !i.pocetnaPripada && !j.krajnjaPripada)
-            return new Interval(i.pocetna, j.krajnja, false, false);
-        if (i.pocetna > j.pocetna && i.krajnja > j.krajnja && !i.pocetnaPripada && j.krajnjaPripada)
-            return new Interval(i.pocetna, j.krajnja, false, true);
-        if (i.pocetna > j.pocetna && i.krajnja > j.krajnja && i.pocetnaPripada && !j.krajnjaPripada)
-            return new Interval(i.pocetna, j.krajnja, true, false);
-        if (i.pocetna < j.pocetna && i.krajnja < j.krajnja && j.pocetnaPripada && i.krajnjaPripada)
-            return new Interval(j.pocetna, i.krajnja, true, true);
-        if (i.pocetna < j.pocetna && i.krajnja < j.krajnja && !j.pocetnaPripada && !i.krajnjaPripada)
-            return new Interval(j.pocetna, i.krajnja, false, false);
-        if (i.pocetna < j.pocetna && i.krajnja < j.krajnja && !j.pocetnaPripada && i.krajnjaPripada)
-            return new Interval(j.pocetna, i.krajnja, false, true);
-        if (i.pocetna < j.pocetna && i.krajnja < j.krajnja && j.pocetnaPripada && !i.krajnjaPripada)
-            return new Interval(j.pocetna, i.krajnja, true, false);
-        return j;
-    }
-
-    @Override
-    public String toString() {
-        return "Interval{" +
-                "pocetna=" + pocetna +
-                ", krajnja=" + krajnja +
-                ", pocetnaPripada=" + pocetnaPripada +
-                ", krajnjaPripada=" + krajnjaPripada +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Interval)) return false;
-        Interval interval = (Interval) o;
-        return Double.compare(interval.pocetna, pocetna) == 0 && Double.compare(interval.krajnja, krajnja) == 0 && pocetnaPripada == interval.pocetnaPripada && krajnjaPripada == interval.krajnjaPripada;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(pocetna, krajnja, pocetnaPripada, krajnjaPripada);
-    }
 }
